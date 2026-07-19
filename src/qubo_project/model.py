c938cf996b6441b2dbce0726e1887093b13c32ea
Simone <massimo.it18@protonmail.com>
2026-07-19 09:37:42 +0200

    first draft of train()

--- src/qubo_project/model.py
+++ src/qubo_project/model.py
@@ -1,12 +1,25 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from preprocessing import ReadCSV
+
 def train(
- classifier: str, # classifier to use
+ classifier: str, # type of classifier to use
  reducedTrain_csv: str, # training dataset
  target_column: str, # target column name
  model_path: str, # saved trained classifier
  metrics_json: str, # file with training statistics
  seed: int = 42,
 ):
-    return Exception("train function is not implemented yet.")
+    # open the csv file and read the data
+    csv,_,_ = ReadCSV(reducedTrain_csv)
+
+    if classifier == "random_forest":
+        clf = RandomForestClassifier(n_estimators=100, random_state=seed)
+    else:
+        raise ValueError("Unsupported classifier type")
+
+    clf.fit(csv[1:, :-1].astype(float), csv[1:, -1].astype(float))
+    
 
 def predict(
  reduced_Test_csv: str, # Input test set
