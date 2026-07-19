a10cce99e7638c5b5af3850f5f6d87540980f641
Simone <massimo.it18@protonmail.com>
2026-07-19 10:12:40 +0200

    model save using joblib

--- src/qubo_project/model.py
+++ src/qubo_project/model.py
@@ -1,6 +1,15 @@
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
-from preprocessing import ReadCSV
+from preprocessing import ReadCSV, SeparateTarget
+import joblib
+
+def PrepareData(csv_file: str, target_column: str):
+    csv = ReadCSV(csv_file)
+    x_data, y_data = SeparateTarget(csv, target_column)
+    x_headers = x_data[0, :]  # Save header row
+    x_data = x_data[1:, :].astype(float)  # Exclude header row and convert to float
+    y_data = y_data[1:, :].astype(float)  # Exclude header row and convert to float
+    return x_data, y_data, x_headers
 
 def train(
  classifier: str, # type of classifier to use
@@ -11,17 +20,15 @@ def train(
  seed: int = 42,
 ):
     classifier = classifier.lower().strip()
-    # open the csv file and read the data
-    csv = ReadCSV(reducedTrain_csv)
-    
-
+    x_train, y_train, x_headers = PrepareData(reducedTrain_csv, target_column)
     if classifier in ["random_forest", "rf", "randomforest", "random forest"]:
         clf = RandomForestClassifier(n_estimators=100, random_state=seed)
     else:
         raise ValueError("Unsupported classifier type")
 
-    clf.fit(csv[1:, :-1].astype(float), csv[1:, -1].astype(float))
-    
+    clf.fit(x_train.astype(float), y_train.astype(float))
+    # Saving classifier using joblib
+    joblib.dump(clf, f'{model_path}/model_{classifier}.pkl')
 
 def predict(
  reduced_Test_csv: str, # Input test set
@@ -30,4 +37,5 @@ def predict(
  predictions_csv: str, # Output predictions
  classif_stats_json: str, # File with classification stats
 ):
-    return Exception("predict function is not implemented yet.")
+    x_test, y_test, x_headers = PrepareData(reduced_Test_csv, target_column)
+
