efa356e6f292eb48af3680084eee3bc957929cd0
Simone <massimo.it18@protonmail.com>
2026-07-19 10:28:15 +0200

    added json stats

--- src/qubo_project/model.py
+++ src/qubo_project/model.py
@@ -2,14 +2,18 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 from preprocessing import ReadCSV, SeparateTarget
 import joblib
+import time
+import json
 
 def PrepareData(csv_file: str, target_column: str):
+    t = time.time()
     csv = ReadCSV(csv_file)
+    tnow = time.time()
     x_data, y_data = SeparateTarget(csv, target_column)
     x_headers = x_data[0, :]  # Save header row
     x_data = x_data[1:, :].astype(float)  # Exclude header row and convert to float
     y_data = y_data[1:, :].astype(float)  # Exclude header row and convert to float
-    return x_data, y_data, x_headers
+    return x_data, y_data, x_headers, tnow - t
 
 def train(
  classifier: str, # type of classifier to use
@@ -20,15 +24,31 @@ def train(
  seed: int = 42,
 ):
     classifier = classifier.lower().strip()
-    x_train, y_train, x_headers = PrepareData(reducedTrain_csv, target_column)
+    x_train, y_train, x_headers, t_in = PrepareData(reducedTrain_csv, target_column)
+    if x_train is None or y_train is None:
+        raise ValueError("Training data could not be prepared. Check the input CSV and target column.")
     if classifier in ["random_forest", "rf", "randomforest", "random forest"]:
+        json_stats["classifier"] = "random_forest"
         clf = RandomForestClassifier(n_estimators=100, random_state=seed)
     else:
         raise ValueError("Unsupported classifier type")
-
+    t = time.time()
     clf.fit(x_train.astype(float), y_train.astype(float))
+    t_fit = time.time() - t
     # Saving classifier using joblib
-    joblib.dump(clf, f'{model_path}/model_{classifier}.pkl')
+    joblib.dump(clf, f'{model_path}')
+    json_stats = {
+        "seed": seed,
+        "model_path": model_path,
+        "training_dataset": reducedTrain_csv,
+        "n_samples": x_train.shape[0],
+        "n_features": x_train.shape[1],
+        "target_1_percentage": np.mean(y_train) * 100,
+        "dataset_input_time": round(t_in, 2),
+        "training_time": round(t_fit, 2),
+    }
+    with open(metrics_json, 'w') as f:
+        json.dump(json_stats, f, indent=4)
 
 def predict(
  reduced_Test_csv: str, # Input test set
@@ -37,5 +57,12 @@ def predict(
  predictions_csv: str, # Output predictions
  classif_stats_json: str, # File with classification stats
 ):
-    x_test, y_test, x_headers = PrepareData(reduced_Test_csv, target_column)
+    x_test, y_test, x_headers, _ = PrepareData(reduced_Test_csv, target_column)
 
+    # Load the trained classifier
+    clf = joblib.load(f'{model_path}')
+    if clf is None:
+        raise ValueError("Classifier not found. Are you sure the model was saved correctly?")
+
+    # Make predictions
+    y_pred = clf.predict(x_test.astype(float))
